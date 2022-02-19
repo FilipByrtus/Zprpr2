5d5d8bbe798381c249e6da21d19f6c4679f01242
FilipByrtus <91073220+FilipByrtus@users.noreply.github.com>
2022-02-19 11:32:20 +0100

    Add files via upload

---
+++ Zprpr2_CV1.1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int n, * d, i, j;
+	scanf_s("%d", &n);
+	if (n < 0 || n > 10) {
+		printf("Postupnost nieje spravna");
+		return(0);
+	}
+	d = (int*) malloc(n * sizeof(int));
+	for (i = 0; i < n; i++) {
+		scanf_s("%d", d + i);
+	}
+	for (i = 1; i < n; i++) {
+		j = i - 1;
+		printf("%d %d\n", d[i], d[j]);
+		if (d[i] > (d[j] * 2) || d[i] < (d[j] / 2)) {
+			printf("Postupnost nieje spravna");
+			return(0);
+		}
+	}
+	printf("Postupnost je spravna");
+}
