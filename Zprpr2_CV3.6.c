7058a312db9516720e9192db6395711cc38af9e3
FilipByrtus <91073220+FilipByrtus@users.noreply.github.com>
2022-03-03 16:22:57 +0100

    Update Zprpr2_CV3.6.c

--- Zprpr2_CV3.6.c
+++ Zprpr2_CV3.6.c
@@ -1,12 +1,48 @@
-#include<stdlib.h>
+/*#include<stdlib.h>
 #include<stdio.h>
 
 int main() {
-	int pocet[25];
-	char pismena[25] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', };
-	char fileName[50];
+	int pocet[26], i, j, f = 1;
+	char pismenaMale[26];
+	char pismenaVelke[26];
+	char fileName[50], loadedChar;
 	FILE* vstup;
 	scanf("%s^\n", fileName);
 	vstup = fopen(fileName, "r");
+	for (i = 0; i < 26; i++) {
+		pismenaMale[i] = i + 97;
+		pismenaVelke[i] = i + 65;
+		pocet[i] = 0;
+	}
+	for (i = 0; i < 26; i++) {
+		printf("%c ", pismenaVelke[i]);
+	}
+	printf("\n");
+	do{
+		while (f) {
+			loadedChar = getc(vstup);
+			for (i = 0; i < 26; i++) {
+				if (loadedChar == pismenaMale[i] || loadedChar == pismenaVelke[i]) {
+					pocet[i] += 1;
+				}
+			}
+			if (loadedChar == '\n'|| loadedChar == EOF) {
+				for (i = 0; i < 26; i++) {
+					if (pocet[i] > 9) {
+						if (pocet[i] > 99) pocet[i] = 99;
+						printf("%d", pocet[i]);
+					}
+					if (pocet[i] < 10) {
+						printf("%d ", pocet[i]);
+					}
+				}
+				printf("\n");
+				for (i = 0; i < 26; i++) {
+					pocet[i] = 0;
+				}
+				if (loadedChar == EOF) break;
+			}
+		}
+	}while (loadedChar != EOF);
 
-}
+}*/
